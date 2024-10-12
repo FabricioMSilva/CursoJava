f38f1d5ec8a73bf2155e80bb5e051a9124059198
FABRICIO MARTINS DA SILVA <87395533+FabricioMSilva@users.noreply.github.com>
2024-10-11 22:09:24 -0300

    Initial commit

---
+++ App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        System.out.println("Informe o quantos Litros deseja:"); 
+        int litros = new java.util.Scanner(System.in).nextInt();
+
+        System.out.println("Valor a ser pago é: " + (litros * 5) + " Reais");
+
+    }
+}
